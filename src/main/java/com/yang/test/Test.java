23b1cf86d158784bcd34c1746fd3242279dfb2d1
ysw <18768905734@163.com>
2022-09-22 20:31:44 +0800

    idea commit first

---
+++ src/main/java/com/yang/test/Test.java
@@ -0,0 +1,11 @@
+package com.yang.test;
+
+/**
+ * @author 杨世伟
+ * @create 2022-09-22-20:25
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
